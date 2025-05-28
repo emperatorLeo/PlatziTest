35757a8c2e7668e41bb861317314dd569d1ad28f
emperatorLeo <leo_svxy@hotmail.com>
2025-05-28 13:52:12 -0500

    adding conflict strategy on insertion

--- app/src/main/java/com/example/platzitest/data/local/dao/PlatziDao.kt
+++ app/src/main/java/com/example/platzitest/data/local/dao/PlatziDao.kt
@@ -2,6 +2,7 @@ package com.example.platzitest.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.platzitest.common.SOUND_TABLE
 import com.example.platzitest.data.model.entities.SoundEntity
@@ -10,7 +11,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface PlatziDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertSound(soundList: List<SoundEntity>)
 
     @Query("SELECT * FROM $SOUND_TABLE")
